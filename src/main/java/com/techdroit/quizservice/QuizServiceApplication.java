4cf1878eac2eee373bec2a36c60ac5b26cc5adbb
Teslim <teslimkuku@gmail.com>
2017-09-18 13:51:04 +0100

    First Commit

---
+++ src/main/java/com/techdroit/quizservice/QuizServiceApplication.java
@@ -0,0 +1,12 @@
+package com.techdroit.quizservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizServiceApplication.class, args);
+	}
+}
