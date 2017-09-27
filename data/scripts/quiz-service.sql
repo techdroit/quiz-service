bf0c6c79f01c0b2705595ceea8746d2125d8be33
Teslim <teslimkuku@gmail.com>
2017-09-27 21:12:06 +0100

    Added question_types definition and quiz_info definition to quiz-services.sql

--- data/scripts/quiz-service.sql
+++ data/scripts/quiz-service.sql
@@ -46,3 +46,40 @@ CREATE TABLE quiz.user_roles (
 ENGINE=InnoDB
 DEFAULT CHARSET=latin1
 COLLATE=latin1_swedish_ci ;
+
+CREATE TABLE quiz.question_types (
+	question_type_id INT NOT NULL AUTO_INCREMENT,
+	question_type_description varchar(100) NOT NULL,
+	PRIMARY KEY(question_type_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1
+COLLATE=latin1_swedish_ci ;
+
+insert into question_types(question_type_description) values('true or false');
+insert into question_types(question_type_description) values('multi choice radio');
+insert into question_types(question_type_description) values('multi choice check');
+
+CREATE TABLE quiz.quiz_info (
+	quiz_id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+	quiz_name varchar(50) NOT NULL,
+	quiz_description varchar(100) NULL,
+	quiz_no_of_total_questions INT NULL,
+	quiz_no_of_questions_allowed INT NULL,
+	status_id TINYINT NOT NULL,
+	quiz_start_date DATE NULL,
+	quiz_end_date DATE NULL,
+	quiz_time_allowed TINYINT NULL,
+	quiz_no_of_attempts TINYINT NULL,
+	quiz_passing_score TINYINT NULL,
+	quiz_question_display_mode TINYINT NULL,
+	quiz_question_selection_mode TINYINT NULL,
+	quiz_mode_id TINYINT NOT NULL,
+	quiz_maker_id TINYINT UNSIGNED NOT NULL,
+	quiz_maker_date DATE NOT NULL,
+	quiz_maker_comment varchar(100) NOT NULL,
+	PRIMARY KEY(quiz_id)
+)
+ENGINE=InnoDB
+DEFAULT CHARSET=latin1
+COLLATE=latin1_swedish_ci ;
